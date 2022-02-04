6fd63440b613cff3854e5c0b561239332134ef5c
Mohamadamin Karami <mohamadaminkarami.ce@gmail.com>
2022-02-04 17:17:22 +0330

    Create Post model

--- myfirstblog/posts/models.py
+++ myfirstblog/posts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
+    body = models.TextField()
+
+    def __str__(self) -> str:
+        return self.title + " | " + str(self.author)
